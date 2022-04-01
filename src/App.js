5ad1eba043e5479ef7f5067d33bdc693458819dd
Mr Robret <bret.farley@gmail.com>
2022-04-01 05:39:41 -0500

    added api keys, ts, and conversion to hash

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import Carousel from './components/Carousel';
 import Header from './components/Header';
 import Card from './UI/Card';
 import './App.css';
+import md5 from 'md5';
 
 import img1 from './images/slide-1.jpeg';
 import img2 from './images/slide-3.jpeg';
 import img3 from './images/slide-2.jpeg';
+
+
+// request URL: https://gateway.marvel.com:443/v1/public/comics?format=comic&noVariants=true&dateDescriptor=thisWeek&orderBy=title&limit=25&apikey=8b985aedc4e35073e7b539df04cf3a31
+
 //const imagesList = importAll(require.context('./images', false, /\.(png|pje?g|svg)$/));
 const App = () => {
   
@@ -17,12 +22,24 @@ const App = () => {
   ];
 
   const [newTitles, setNewTitles] = useState(['Option1', 'Option2', 'Option3']); //get from API
+
+  const privateKey = process.env.REACT_APP_API_SECRET;
+  const publicKey = process.env.REACT_APP_API_PUBLIC;
   
-  // useEffect(() => {
-  //   fetch("https://gateway.marvel.com:443/v1/public/comics?orderBy=onsaleDate&limit=25&apikey=8b985aedc4e35073e7b539df04cf3a31")
-  //   .then(response => response.json())
-  //   .then(data => setNewTitles(data.message));
-  // }, []);
+  useEffect(() => {
+    
+    const currentTimeStamp = Date.now().toString();
+    const message = currentTimeStamp + privateKey + publicKey;
+    const hash = md5(message);
+    console.log(hash);
+    console.log(typeof(hash));
+  //   hash.update(currentTimeStamp + privateKey + publicKey);
+    const requestTitles = `https://gateway.marvel.com:443/v1/public/comics?&ts=${currentTimeStamp}&format=comic&noVariants=true&dateDescriptor=thisWeek&orderBy=title&limit=25&apikey=${publicKey}&hash=${hash}`;
+    
+    fetch(requestTitles)
+    .then(response => response.json())
+    // .then(data => setNewTitles(data.message));
+  }, []);
 
   // const [currentTitle, setCurrentTitle] = useState(titlesList[0]);
   const [singleIssueCovers, setSingleIssueCovers] = useState(defaultList);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// const connection = () => {
+//   const xhr = new XMLHttpRequest()
+//   xhr.open('GET', './connections/get-data', true)
+//   xhr.onloadstart = () => {
+
+//   }
+//   xhr.onerror = () => {
+
+//   }
+//   xhr.onload = () => {
+//     if (this.status == 200) {
+//       console.log(this.textResponse);
+//     } else {
+
+//     }
+//   }
+//   xhr.onloadend = () => {
+
+//   }
+//   xhr.send;
+// }
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
