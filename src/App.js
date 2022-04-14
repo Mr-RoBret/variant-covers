861a8e62749c1caddccc67583435272e1c535eca
Mr Robret <bret.farley@gmail.com>
2022-04-14 17:03:38 -0500

    refactored useEffects in App

--- src/App.js
+++ src/App.js
@@ -15,56 +15,33 @@ import img3 from './images/slide-2.jpeg';
 //const imagesList = importAll(require.context('./images', false, /\.(png|pje?g|svg)$/));
 const App = () => {
   
-  const defaultList = [ //get from API
+  const defaultList = [
         {key: img1, src: img1}, 
         {key: img2, src: img2}, 
         {key: img3, src: img3}
   ];
 
-  //get from API
   const [newTitles, setNewTitles] = useState([]); 
   const [singleIssueCovers, setSingleIssueCovers] = useState(defaultList);
+  const [currentTitleID, setCurrentTitleID] = useState('');
 
   const privateKey = process.env.REACT_APP_API_SECRET;
   const publicKey = process.env.REACT_APP_API_PUBLIC;
-
-  const formatImageName = (response) => {
-    const fileName = response.data.results[0].images[0].path;
-    const fileExtension = response.data.results[0].images[0].extension;
-    const fullName = fileName + '.' + fileExtension;
-    console.log(`file's full name is ${fullName}.`);
-    return (fullName);
-  }
-
-  const getSingleVariant = (cover) => {
-    const coverImage = cover.resourceURI.split("/").pop();
-    console.log(coverImage);
-    // request API again w/ variant image ID
-    const currentTimeStamp = Date.now().toString();
-    const message = currentTimeStamp + privateKey + publicKey;
-    const hash = md5(message);
-    
-    // hash.update(currentTimeStamp + privateKey + publicKey);
-    const requestVariantImage = `https://gateway.marvel.com:443/v1/public/comics/${coverImage}?&ts=${currentTimeStamp}&apikey=${publicKey}&hash=${hash}`;
-    
-    return (fetch(requestVariantImage)
-    .then(response => response.json())
-    .then(data => formatImageName(data)))
-  }
-
-  const parseData = (response) => {
-    const titlesArr = response.data.results.map(item => item.title);
-    const coversArr = response.data.results[0].variants.map(cover => (getSingleVariant(cover)));
-    console.log(titlesArr);
-    setNewTitles(titlesArr);
-    setSingleIssueCovers(coversArr);
-  }
-
+  
   useEffect(() => {
     const currentTimeStamp = Date.now().toString();
     const message = currentTimeStamp + privateKey + publicKey;
     const hash = md5(message);
     
+    const parseData = (response) => {
+      const titlesArr = response.data.results.map(item => item.title);
+      const titleID = response.data.results[0].id;
+      console.log(titlesArr);
+      setNewTitles(titlesArr);
+      setCurrentTitleID(titleID);
+    }
+
+
     // hash.update(currentTimeStamp + privateKey + publicKey);
     const requestTitles = `https://gateway.marvel.com:443/v1/public/comics?&ts=${currentTimeStamp}&format=comic&noVariants=false&dateDescriptor=thisWeek&orderBy=title&limit=25&apikey=${publicKey}&hash=${hash}`;
     
@@ -73,11 +50,41 @@ const App = () => {
     .then(data => parseData(data));
   }, []);
 
+  useEffect(() => {
+    // request API again w/ variant image ID
+    const currentTimeStamp = Date.now().toString();
+    const message = currentTimeStamp + privateKey + publicKey;
+    const hash = md5(message);
+
+    const coverImage = currentTitleID;
+    console.log(coverImage);
+
+    const formatImageName = (response) => {
+      const fileName = response.data.results[0].images[0].path;
+      const fileExtension = response.data.results[0].images[0].extension;
+      const fullName = fileName + '.' + fileExtension;
+      console.log(`file's full name is ${fullName}.`);
+      return (fullName);
+    }
+
+    const parseData = (response) => {
+      const coversArr = response.data.results[0].variants.map(cover => (formatImageName(cover)));
+      setSingleIssueCovers(coversArr);
+    }
+    
+    // hash.update(currentTimeStamp + privateKey + publicKey);
+    const requestVariantImage = `https://gateway.marvel.com:443/v1/public/comics/${coverImage}?&ts=${currentTimeStamp}&apikey=${publicKey}&hash=${hash}`;
+    
+    fetch(requestVariantImage)
+    .then(response => response.json())
+    .then(data => parseData(data))
+  }, [privateKey, publicKey, currentTitleID]);
+
   const handleSelectedTitle = (newTitle) => {
     console.log(newTitle);
     // send newTitle to API and request covers
     // setCurrentTitle(newTitle);
-    setSingleIssueCovers(defaultList);
+    setCurrentTitleID(newTitle);
   };
 
   return (
