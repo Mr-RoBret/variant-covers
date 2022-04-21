7832936b613e950d8999aa9834f6eafe4c847d6e
Mr Robret <bret.farley@gmail.com>
2022-04-21 13:00:23 -0500

    refactored components to distribute state mgmt

--- src/App.js
+++ src/App.js
@@ -5,131 +5,113 @@ import Card from './UI/Card';
 import './App.css';
 import md5 from 'md5';
 
-import img1 from './images/slide-1.jpeg';
-import img2 from './images/slide-3.jpeg';
-import img3 from './images/slide-2.jpeg';
-
 /**
  * App Component
  * @returns ...
  */
 
 const App = () => {
-  
-  // static list of covers from "images" folder
-  const defaultList = [
-        {key: img1, src: img1}, 
-        {key: img2, src: img2}, 
-        {key: img3, src: img3}
-  ];
 
   // declare useState variables
-  const [newTitles, setNewTitles] = useState([]); 
-  const [currentTitle, setCurrentTitle] = useState('')
-  const [singleIssueCovers, setSingleIssueCovers] = useState(defaultList);
-  const [currentTitleID, setCurrentTitleID] = useState('');
-
-  // private and public key variables
-  const privateKey = process.env.REACT_APP_API_SECRET;
-  const publicKey = process.env.REACT_APP_API_PUBLIC;
-
-  /** 
-   ********************************* UseEffect #1 *********************************
-   */
+  const [newTitleArr, setNewtitleArr] = useState([]);
+  const [newTitleID, setNewTitleID] = useState('');
+  const [variantIDs, setVariantIDs] = useState([]);
+  const [variantCovers, setVariantCovers] = useState([]);
   
-  useEffect(() => {
-    // create API fetch request params
-    const currentTimeStamp = Date.now().toString();
-    const message = currentTimeStamp + privateKey + publicKey;
-    const hash = md5(message);
-    
-    // parseData takes API data and maps week's titles into array of titles
-    const parseData = (response) => {
-
-      // map this week's titles into new array and setNewTitles to array
-      const titlesArr = response.data.results.map(item => item.title
-      );
-      
-      setNewTitles(titlesArr);
-      
-      // setCurrentTitleID to ID of first issue in list
-      const titleID = response.data.results[0].id.toString();
-      setCurrentTitleID(titleID);
-      console.log(`currentTitleID is ${currentTitleID} and its type is ${typeof(currentTitleID)}`);
-    }
-      
-
-    // hash.update(currentTimeStamp + privateKey + publicKey);
-    const requestTitles = `https://gateway.marvel.com:443/v1/public/comics?&ts=${currentTimeStamp}&format=comic&noVariants=false&dateDescriptor=thisWeek&orderBy=title&limit=25&apikey=${publicKey}&hash=${hash}`;
-
-    console.log(`reqeustTitles is ${requestTitles}`);
-    
-    // fetch list of titles from last week and send data to parseData function
-    fetch(requestTitles)
-    .then(response => response.json())
-    .then(data => parseData(data));
-  }, []);
-
   /** 
    ********************************* UseEffect #2 *********************************
+   * this side effect will occur upon selection of single title from dropdown 
+   * list of this week's issues.
+   */
+  useEffect(() => {
+    // private and public key variables
+    const privateKey = process.env.REACT_APP_API_SECRET;
+    const publicKey = process.env.REACT_APP_API_PUBLIC;
+
+    const currentTimeStamp = Date.now().toString();
+    const message = currentTimeStamp + privateKey + publicKey;
+    const hash = md5(message);
+    // get ID of currently selected issue
+    const newTitleID = newTitleArr[0];
+    
+    // get IDs of variants
+    const getIDs = (coverURI) => {
+      const coverIdArr = coverURI.split('/');
+      return coverIdArr[coverIdArr.length-1]
+    }
+    
+    // get Variants from response
+    const getVariantIDs = (response) => {  
+      // create new array from mapping fetched variants to getIDs(resoureURI).
+      const coverIDsArr = response.data.results[0].variants.map(cover => (getIDs(cover.resourceURI)));
+
+      // set variant covers to images retrieved (how to retrieve images? format file name and extension and return)
+      setVariantIDs(coverIDsArr);
+    }
+
+    // hash.update(currentTimeStamp + privateKey + publicKey);
+    const requestVariants = `https://gateway.marvel.com:443/v1/public/comics/${newTitleID}?&ts=${currentTimeStamp}&apikey=${publicKey}&hash=${hash}`;
+    
+    fetch(requestVariants)
+    .then(response => response.json())
+    .then(data => getVariantIDs(data))
+  }, [newTitleArr]);
+
+  /** 
+   ********************************* UseEffect #3 *********************************
+   * runs when list of variant IDs is obtained from UseEffect #2 and variantCovers
+   * state changes.
    */
 
-  // this side effect will occur upon selection of single title from dropdown list
-  // of this week's issues.
-
   useEffect(() => {
+    // private and public key variables
+    const privateKey = process.env.REACT_APP_API_SECRET;
+    const publicKey = process.env.REACT_APP_API_PUBLIC;
+
     const currentTimeStamp = Date.now().toString();
     const message = currentTimeStamp + privateKey + publicKey;
     const hash = md5(message);
 
-    // get ID of currently selected issue
-
-
-    const coverImageID = currentTitleID; // this is wrong... only grabs the original first issue in the list's ID... needs to grab the selected issue's id, which can be handed up through the chain from where it's selected in the dropdown.?? Or can we grab the correct ID from the top level?
-
-
-    console.log(`coverImageID is ${coverImageID} and its type is ${typeof(coverImageID)}. (line 91)`);
-
+    // formats image name and extension and returns to parseData
     const formatImageName = (cover) => {
-      console.log(`cover is ${cover}`);
       const fileName = cover.path;
+      console.log(`fileName is ${fileName}`)
       const fileExtension = cover.extension;
+      console.log(`fileExtension is ${fileExtension}`)
       const fullName = fileName + '.' + fileExtension;
-      console.log(`file's full name is ${fullName}.`);
+
       return (fullName);
     }
 
-    const parseData = (response) => {
-      // console.log(response.data.variants[0]);
-      const coversArr = response.data.variants[0].map(cover => (formatImageName(cover)));
-      console.log(coversArr);
-      setSingleIssueCovers(coversArr);
+    // maps over variants requested from api and sends to format
+    const parseData = (variants) => {
+      const formattedVars = variants.map(cover => 
+        (formatImageName(cover))
+      );
+      setVariantCovers(formattedVars);
     }
-    console.log(`coverImageID sent to request is: ${coverImageID}`);
-
-    // hash.update(currentTimeStamp + privateKey + publicKey);
-    const requestVariantImage = `https://gateway.marvel.com:443/v1/public/comics/${coverImageID}?&ts=${currentTimeStamp}&apikey=${publicKey}&hash=${hash}`;
-    console.log(requestVariantImage);
     
-    fetch(requestVariantImage)
-    .then(response => response.json())
-    .then(data => parseData(data))
-  }, [currentTitle]);
+    // fetch data for images (and extensions) correlating with the above array
+    // ** FETCH IMAGE DATA HERE FOR FORMATTING? **
+    const requestTitles = `https://gateway.marvel.com:443/v1/public/comics?&ts=${currentTimeStamp}&format=comic&noVariants=false&dateDescriptor=thisWeek&orderBy=title&limit=25&apikey=${publicKey}&hash=${hash}`;
+    
+    // fetch list of titles from last week and send data to parseData function
+    fetch(requestTitles)
+      .then(response => response.json())
+      .then(data => parseData(data));
+  }, [variantIDs]);
 
-
-  // event handler for selected title from dropdown
-  // sets currentTitle to the title selected
-  const handleSelectedTitle = (newTitle) => {
-    console.log(`from handleSelectedTitle: newTitle is ${newTitle}`);
-    setCurrentTitle(newTitle);
-  };
+  // handle selected option from Header/Dropdown
+  const handleSelectedTitle = (newTitleObj) => {
+    setNewtitleArr(newTitleObj);
+  }
 
   return (
     <div>
       <div>
-        <Header titlesList={newTitles} onChange={handleSelectedTitle} />
+        <Header onChange={handleSelectedTitle} />
         <Card>
-            <Carousel covers={singleIssueCovers} />
+            <Carousel covers={variantCovers} />
         </Card>
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,74 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import SubmitButton from "../UI/SubmitButton";
 import DropDownOptions from "../UI/DropDownOptions";
 import styles from './Header.module.css';
+import md5 from 'md5';
+
 
 const Header = (props) => {
 
-    const [currentTitle, setCurrentTitle] = useState(props.titlesList[0]);
+    const [currentObjects, setCurrentObjects] = useState([]);
+    const [currentTitleObj, setCurrentTitleObj] = useState('');
+    const [newTitles, setNewTitles] = useState([]); 
+    const [currentTitleID, setCurrentTitleID] = useState('');
 
-    const handleSelectedTitle = (newTitle) => {
-        console.log(`from handleSelectedTitle in Header: newTitle is ${newTitle}`);
-        setCurrentTitle(newTitle);
-        props.onChange(newTitle);
+    /** 
+   ********************************* UseEffect #1 *********************************
+   * fetches latest title info and turns them into React-readable data (array?).
+   * then sends titles to dropdown, which, upon a selection, hands the info up to 
+   * App, so its variants can be selected
+   */
+  
+  useEffect(() => {
+    const privateKey = process.env.REACT_APP_API_SECRET;
+    const publicKey = process.env.REACT_APP_API_PUBLIC;
+    // create API fetch request params
+    const currentTimeStamp = Date.now().toString();
+    const message = currentTimeStamp + privateKey + publicKey;
+    const hash = md5(message);
+    
+    // parseData takes API data and maps week's titles into array of titles
+    const parseData = (response) => {
+
+      // map this week's titles into new array and setNewTitles to array
+        const itemsArr = response.data.results.map((item) => {
+            const itemArr = [];
+            return (
+                itemArr.push(item.title, item.id)
+            )
+        }
+        );
+        setCurrentObjects(itemsArr);
+
+      const titlesArr = itemsArr.map(item => item[0]);
+      setNewTitles(titlesArr);
+      console.log(`newTitles, after extracting from itemsArr, is: ${newTitles}.`);
+
+      // setCurrentTitleID to ID of first issue in list
+    //   const titleID = response.data.results[0].id.toString();
+    //   setCurrentTitleID(titleID);
+    }
+
+    // hash.update(currentTimeStamp + privateKey + publicKey);
+    // ** initial request URL **
+    const requestTitles = `https://gateway.marvel.com:443/v1/public/comics?&ts=${currentTimeStamp}&format=comic&noVariants=false&dateDescriptor=thisWeek&orderBy=title&limit=25&apikey=${publicKey}&hash=${hash}`;
+    
+    // fetch list of titles from last week and send data to parseData function
+    fetch(requestTitles)
+    .then(response => response.json())
+    .then(data => parseData(data));
+  },[newTitles]);
+
+  const handleSelectedTitle = (newTitle) => {
+    console.log(`from handleSelectedTitle in Header: newTitle is ${newTitle}`);
+    setCurrentTitleObj(newTitle);
+    for (let item in currentObjects) {
+        if (currentObjects[item].includes(newTitle)) {
+            setCurrentTitleObj([item.id, item.title]);
+        }
     };
+    props.onChange(currentTitleObj);
+  };
     
     return (  
         <div className={styles.header}>
@@ -21,7 +78,7 @@ const Header = (props) => {
             </div>
             <div className={styles.selection}>
                 <h2 className={styles.subTitle}>Select a Title</h2>
-                <DropDownOptions options={props.titlesList} onChange={handleSelectedTitle} />
+                <DropDownOptions options={newTitles} onChange={handleSelectedTitle} />
             </div>
             {/* <SubmitButton onSelected={handleSelectedTitle}/> */}
         </div>
