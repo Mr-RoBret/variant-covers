10425c04d66cbfc29e31b094634c484356fd5ba7
Mr Robret <bret.farley@gmail.com>
2022-05-01 16:07:20 -0500

    "one off" error fixed

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -20,50 +20,49 @@ const Carousel = (props) => {
 
     const nextSlide = () => {
         if (currentIndex === newCovers.length - 1) {
-            console.log(`currentIndex === newCovers.length - 1`);
+            // console.log(`currentIndex === newCovers.length - 1`);
             return (setState({
                 ...state,
                 translate: 'translate(0px)',
                 currentIndex: 0
             }))
         }
-        console.log('currentIndex !== newCovers.length - 1');
+        // console.log('currentIndex !== newCovers.length - 1');
         setState({
             ...state,
             currentIndex: currentIndex + 1,
             translate: `translate(${(currentIndex + 1) * -455}px)`
         })
-        console.log(`translate: ${translate}`);
+        // console.log(`translate: ${translate}`);
         // return (`translate: ${translate}px`);
     }
 
     const prevSlide = () => {
         if (currentIndex === 0) {
-            console.log(`currentIndex === 0`);
+            // console.log(`translate: ${translate}`);
             return (setState({
                 ...state,
-                translate: `translate(${(newCovers.length - 1) * 455}px)`,
+                translate: `translate(${(newCovers.length - 1) * -455}px)`,
                 currentIndex: newCovers.length - 1
             }))
         }
-        console.log('currentIndex != 0');
         setState({
             ...state,
             currentIndex: currentIndex - 1,
-            translate: `translate(${(currentIndex - 1) * 455}px)`
+            translate: `translate(${(currentIndex - 1) * -455}px)`
         })
     }
 
     const moveContentLeft = () => {
         console.log('move to next slide');
         nextSlide();
-        // console.log(`translate is ${translate}`);
+        console.log(`translate is ${translate}`);
     }
 
     const moveContentRight = () => {
         console.log('move to prev slide');
         prevSlide();
-        // console.log(`translate is ${translate}`);
+        console.log(`translate is ${translate}`);
     }
 
     return (
