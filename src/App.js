21f8d6f245f05521d1bd8f4481bf7122ea12f5d2
Bret Farley <34036924+Mr-RoBret@users.noreply.github.com>
2024-02-03 11:10:22 -0600

    adds accurate dateRange and fixes (reverses) cover order

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
   const [variantCovers, setVariantCovers] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [coverWidth, setCoverWidth] = useState(window.innerWidth);
- 
+
   const privateKey = process.env.REACT_APP_API_SECRET;
   const publicKey = process.env.REACT_APP_API_PUBLIC;
 
@@ -38,31 +38,31 @@ const App = () => {
       // 4. get IDs of variants
       const getIDs = (cover) => {
         const coverID = cover.resourceURI.split('/');
-        return coverID[coverID.length-1];
+        return coverID[coverID.length - 1];
       }
-      
+
       // 3. get Variants from response and set variantIDs (state);
       // this will trigger next useEffect (below).
-      const getVariantIDs = (res) => {  
+      const getVariantIDs = (res) => {
         // create new array from mapping fetched variants to getIDs(resoureURI).
         let newIDs = res.data.results[0].variants.map((cover) => getIDs(cover));
         newIDs.push(newTitleID);
         setVariantIDs(newIDs);
       }
-  
+
       // 2. function to construct API call url with either initial ID or new ID
       const constructRequestURL = (titleID) => {
-        
+
         const requestVariants = `https://gateway.marvel.com:443/v1/public/comics/${titleID}?&ts=${currentTimeStamp}&apikey=${publicKey}&hash=${hash}`;
-        
-        const fetchData = async() => {
+
+        const fetchData = async () => {
           const data = await fetch(requestVariants);
           const json = await data.json();
           getVariantIDs(json);
         }
-        
+
         fetchData()
-        .catch(console.error);;
+          .catch(console.error);;
       }
 
       // 1. if there is a newTitleID returned, call constructRequest function
@@ -82,23 +82,23 @@ const App = () => {
 
   useEffect(() => {
     // console.log(firstRender);
-    
+
     const getArtistInfo = (creators) => {
       let artistIndex = null;
       if (creators.items.find(item => item.role === 'penciler (cover)')) {
-          artistIndex = creators.items.findIndex(item => item.role === 'penciler (cover)');
-          console.log(artistIndex);
-          return creators.items[artistIndex].name;
+        artistIndex = creators.items.findIndex(item => item.role === 'penciler (cover)');
+        console.log(artistIndex);
+        return creators.items[artistIndex].name;
       }
       else if (creators.items.find(item => item.role === 'painter (cover)')) {
-          artistIndex = creators.items.findIndex(item => item.role === 'painter (cover)');
-          console.log(artistIndex);
-          return creators.items[artistIndex].name;
+        artistIndex = creators.items.findIndex(item => item.role === 'painter (cover)');
+        console.log(artistIndex);
+        return creators.items[artistIndex].name;
       }
       else if (creators.items.find(item => item.role === 'colorist (cover)')) {
-          artistIndex = creators.items.findIndex(item => item.role === 'colorist (cover)');
-          console.log(artistIndex);
-          return creators.items[artistIndex].name;
+        artistIndex = creators.items.findIndex(item => item.role === 'colorist (cover)');
+        console.log(artistIndex);
+        return creators.items[artistIndex].name;
       } else {
         return "artist unavailable";
       }
@@ -106,7 +106,7 @@ const App = () => {
     }
 
     if (!firstRender) {
-      
+
       // 5. Formats image name and extension and returns
       const formatImageName = (data) => {
         const fileName = data.data.results[0].thumbnail.path;
@@ -121,18 +121,18 @@ const App = () => {
         const imageAndArtist = [fileName + '.' + fileExtension, artistName]
         return imageAndArtist;
       };
-  
+
       // 2. function to dynamically replace comic ID# with ID passed in
       const requestVariantCovers = ((individualVariantID) => {
         return (`https://gateway.marvel.com:443/v1/public/comics/${individualVariantID}?&ts=${currentTimeStamp}&apikey=${publicKey}&hash=${hash}`);
       });
-  
+
       // 1. Takes array of variant comic ID#s and maps to new array of request urls
       // (via calling requestVariantCovers function on each item)
       const variantURLs = variantIDs.map((item) => {
         return (requestVariantCovers(item));
       });
-      
+
       // 4. Async function that passes data to formatting function and returns result
       async function getVariantCovers(item) {
         const response = await fetch(item);
@@ -140,13 +140,13 @@ const App = () => {
 
         return formatImageName(data);
       }
-      
+
       // 3. Takes array of request urls and passes to async function
       // (getVariantCovers) for formatting; returns array of file names
       const returnedCovers = variantURLs.map((item) => {
         return getVariantCovers(item)
       });
-      
+
       // 5. Once promise (returnedCovers) has been fulfilled, pushes items to
       // itemsArray and sets variantCovers to itemsArray
       Promise.allSettled(returnedCovers).then((items) => {
@@ -157,7 +157,7 @@ const App = () => {
           /** 
            * extract correct artist value (from around line 88 above) 
            */
-          itemsArray.push({key: index, value: item.value[0], artist: item.value[1]});
+          itemsArray.unshift({ key: index, value: item.value[0], artist: item.value[1] });
           index++;
         }
         setVariantCovers(itemsArray);
@@ -184,11 +184,11 @@ const App = () => {
       <div>
         <Header onChange={handleSelectedTitle} onLoad={handleInitialTitle} />
         <div>
-            <IndexContext.Provider value={{
-                  currentIndex: currentIndex,
-                }}>
-              <Carousel covers={variantCovers} vw={coverWidth} />
-            </IndexContext.Provider>
+          <IndexContext.Provider value={{
+            currentIndex: currentIndex,
+          }}>
+            <Carousel covers={variantCovers} vw={coverWidth} />
+          </IndexContext.Provider>
         </div>
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,25 +6,39 @@ import logo from "../images/title_logo.png";
 
 const Header = (props) => {
 
-    const [newTitles, setNewTitles] = useState([]); 
+    const [newTitles, setNewTitles] = useState([]);
     const initialTitleID = useRef('');
     const itemsArr = useRef([]);
-    
+
     /** 
      * useEffect fetches latest title info and turns into React-readable object.
      * Then sends titles to dropdown, which, upon a selection, hands the info up to 
      * App, so its variants can be selected
      */
-    
-    
+
+
     useEffect(() => {
         const privateKey = process.env.REACT_APP_API_SECRET;
         const publicKey = process.env.REACT_APP_API_PUBLIC;
         // create API fetch request params
         const currentTimeStamp = Date.now().toString();
+
+        const currentDate = new Date().toISOString().split('T')[0];
+        const conversDate = (new Date().toUTCString());
+        const prevMonthDate = new Date(conversDate);
+        prevMonthDate.setMonth(prevMonthDate.getMonth() - 1);
+        const prevDate = prevMonthDate.toISOString().split('T')[0];
+
+        console.log(currentDate);
+        console.log(prevDate);
+
+        const dateRange = `${prevDate}, ${currentDate}`;
+        console.log(dateRange);
+        console.log(typeof (dateRange));
+
         const message = currentTimeStamp + privateKey + publicKey;
         const hash = md5(message);
-        
+
         // parseData takes API data and maps week's titles into array of titles
         const parseData = (response) => {
 
@@ -33,8 +47,8 @@ const Header = (props) => {
 
             // map this week's titles into new array and setNewTitles to array
             itemsArr.current = itemsArr.current.concat(newArray.map((item) => {
-                    return {id: item.id, title: item.title};
-                }
+                return { id: item.id, title: item.title };
+            }
             ));
 
             initialTitleID.current = itemsArr.current[0].id;
@@ -44,49 +58,50 @@ const Header = (props) => {
                 titlesArr.push(itemsArr.current[i].title);
             }
 
-        setNewTitles(titlesArr);
-        // console.log(`newTitles, after extracting from itemsArr, are: ${titlesArr}.`);
-        // console.log(`initialTitleID is ${initialTitleID.current}`);
-        props.onLoad(initialTitleID.current);
+            setNewTitles(titlesArr);
+            // console.log(`newTitles, after extracting from itemsArr, are: ${titlesArr}.`);
+            // console.log(`initialTitleID is ${initialTitleID.current}`);
+            props.onLoad(initialTitleID.current);
         }
 
-    // ** initial request URL **
-    const requestTitles = `https://gateway.marvel.com:443/v1/public/comics?&ts=${currentTimeStamp}&format=comic&noVariants=false&dateDescriptor=thisMonth&orderBy=title&limit=100&apikey=${publicKey}&hash=${hash}`;
-    
-    // fetch list of titles from last week and send data to parseData function
-    fetch(requestTitles)
-    .then(response => response.json())
-    .then(data => parseData(data));
-  }, []);
+        // ** initial request URL **
+        // const requestTitles = `https://gateway.marvel.com:443/v1/public/comics?&ts=${currentTimeStamp}&format=comic&noVariants=false&dateDescriptor=thisMonth&orderBy=title&limit=100&apikey=${publicKey}&hash=${hash}`;
+        const requestTitles = `https://gateway.marvel.com:443/v1/public/comics?&ts=${currentTimeStamp}&format=comic&noVariants=false&dateRange=${dateRange}&orderBy=title&limit=100&apikey=${publicKey}&hash=${hash}`;
 
-  if (newTitles === undefined) {
-    return <p>Still loading...</p>;
-  }
+        // fetch list of titles from last week and send data to parseData function
+        fetch(requestTitles)
+            .then(response => response.json())
+            .then(data => parseData(data));
+    }, []);
 
-  const handleSelectedTitle = (newTitle) => {
-    
-    const currentTitleObj = [];
-    let currentTitleID = '';
-    
-    for (let item in itemsArr.current) {
-        if (itemsArr.current[item].title === newTitle) {
-            const id = JSON.stringify(itemsArr.current[item].id);
-            const title = itemsArr.current[item].title;
-            currentTitleObj.push({id: id, title: title});
-            currentTitleID = id;
-        }
+    if (newTitles === undefined) {
+        return <p>Still loading...</p>;
+    }
+
+    const handleSelectedTitle = (newTitle) => {
+
+        const currentTitleObj = [];
+        let currentTitleID = '';
+
+        for (let item in itemsArr.current) {
+            if (itemsArr.current[item].title === newTitle) {
+                const id = JSON.stringify(itemsArr.current[item].id);
+                const title = itemsArr.current[item].title;
+                currentTitleObj.push({ id: id, title: title });
+                currentTitleID = id;
+            }
+        };
+        props.onChange(currentTitleObj, currentTitleID);
+
     };
-    props.onChange(currentTitleObj, currentTitleID);
-    
-  };
-    
-    return (  
+
+    return (
         <div className={styles.header}>
             <div className={styles.headerElements}>
                 {/* <div className={styles.headerLine}></div> */}
                 {/* <h1 className={styles.mainTitle}>The New Variants</h1> */}
                 <div className={styles.headerLogo}>
-                    <img src={logo}  alt="the new variants logo"></img>
+                    <img src={logo} alt="the new variants logo"></img>
                 </div>
             </div>
             <div className={styles.selection}>
